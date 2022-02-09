6fc80e5e365001e24103a8fc672488e40f24c50e
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2022-02-09 14:39:01 -0800

    Update app.py

--- app.py
+++ app.py
@@ -24,3 +24,4 @@ def check_image(img_path):
     # rgb = cv2.imread(img_path)
     # pred = simple_cmfd_decoder( busterNetModel, rgb )
     # figure = visualize_result( rgb, pred, pred, figsize=(20,20), title='BusterNet CMFD')
+    # st.pyplot(figure)
