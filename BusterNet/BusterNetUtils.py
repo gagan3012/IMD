f5975252346c0327852674fbca19bc3f9279100a
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2022-02-09 14:42:53 -0800

    Update BusterNetUtils.py

--- BusterNet/BusterNetUtils.py
+++ BusterNet/BusterNetUtils.py
@@ -5,3 +5,8 @@ from matplotlib import pyplot
 def simple_cmfd_decoder(busterNetModel, rgb):
     """A simple BusterNet CMFD decoder"""
     # 1. expand an image to a single sample batch
+    single_sample_batch = np.expand_dims(rgb, axis=0)
+    # 2. perform busterNet CMFD
+    pred = busterNetModel.predict(single_sample_batch)[0]
+    return pred
+
